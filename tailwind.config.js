1c369598213e819e1e503086e5d61b89b14de3dc
syiffanoriza <syiffa.noriza@gmail.com>
2024-11-12 13:40:25 +0700

    Configuring project, add necessary packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Poppins',
+      lato: 'Montserrat'
+    },
+    extend: {
+      colors: {
+        // DEFAULT BG COLOR
+        'default': '#D8DDE3',
+        primary: {
+          '900': '#145A66',
+          '800': '#2C6B76',
+          '700': '#437B85',
+          '600': '#5B8C94',
+          '500': '#729CA3',
+          '400': '#89ACB2',
+          '300': '#A1BDC2',
+          '200': '#B9CED1',
+          '100': '#D0DEE0',
+          '050': '#E8EFF0',         
+        },
+      },
+    },
   },
   plugins: [],
 }
-
